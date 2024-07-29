4786da1eb7e867adac0b7a088fc6433ee02c71b6
Magnus Hodgson <mdh11747@uga.edu>
2024-07-28 23:22:10 -0400

    Additional contact added

--- js/contact.js
+++ js/contact.js
@@ -9,14 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then(response => response.text())
             .then(data => {
-                document.getElementById("form-container").style.display = "none";
+                document.getElementById("contact-container").style.display = "none";
                 document.getElementById("result-container").style.display = "flex";
                 const success = document.getElementsByClassName("success");
                 success[0].style.display = "block";
                 success[1].style.display = "block";
             })
             .catch(error => {
-                document.getElementById("form-container").style.display = "none";
+                document.getElementById("contact-container").style.display = "none";
                 document.getElementById("result-container").style.display = "flex";
                 const failure = document.getElementsByClassName("failure");
                 failure[0].style.display = "block";
